28b76f15532c8673a1393aafb09fa325feb660a2
nikoada <nikushac@gmail.com>
2018-05-28 00:08:58 +0200

    email verification!

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
-console.log("Hallo world!");
+function p(data) {
+  console.log(data);
+}
 
-<<<<<<< HEAD
-// here we go
-=======
-// starting coding
->>>>>>> 06d0af6066843ff570770dd37ac74e7d2a050a54
+function email_validation(email = prompt("Enter your email please!")) {
+  var splitedEmailArray = email.split('@');
+  var splitedDomain = splitedEmailArray[1].split('.');
+  var domainName = splitedDomain[0];
+  var domainEnding = splitedDomain[splitedDomain.length - 1];
+
+  if (splitedEmailArray.length != 2) {
+    p('Invalid entry!');
+    return false;
+  }
+  else if (Number(splitedEmailArray[0]) == splitedEmailArray[0]) {
+    p('Only numbers!');
+    return false;
+  }
+  else if (splitedEmailArray[0].indexOf('$') != -1 || splitedEmailArray[0].indexOf('%') != -1) {
+    p('Wrong simbols!');
+    return false;
+  }
+  else if (domainName.length < 5 || domainName.length > 32) {
+    p('Domain name invalid!');
+    return false;
+  }
+  else if (   domainEnding != "com"
+           && domainEnding != "de"
+           && domainEnding != "org") {
+    p('We accept emails only with .com .de .org ending - sorry!');
+    return false;
+  }
+  else {
+    p("Email accepted");
+    return true;
+  }
+}
+
+email_validation();
