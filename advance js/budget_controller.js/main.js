7431460990532ab5f23fa9420f63827c66d9b477
Niko <nikushac@gmail.com>
2018-06-26 15:45:04 +0200

    project starting

--- advance js/budget_controller.js/main.js
+++ advance js/budget_controller.js/main.js
@@ -1,21 +1,57 @@
+var switchBtnObj = document.getElementById("switchBtn");
+
+switchBtnObj.addEventListener("click", function() {
+  if (switchBtnObj.innerHTML == "+") {
+    switchBtnObj.innerHTML = "-";
+  } else {
+    switchBtnObj.innerHTML = "+";
+  }
+});
+
+console.log("hello")
+
 
 var budgetController = (function() {
 
-})();
-
-var UIController = (function() {
-  
-  var DOMStrings = {
-    inputType = "add__type";
-  }
+  var income = 0;
 
   return {
-    getInput : function() {},
-    addListItem : function()  {};
-
+    addIncome : function(value) { income += Number(value) },
+    actualIncome : function() { return income; }
   }
-})();
-
-var controller = (function(budgetControl, ) {
 
 })();
+
+
+var UIController = (function() {
+  var incomeDisObj = document.getElementById("income");
+  var inputDes = document.getElementById("inputDes");
+  var inputVal = document.getElementById("inputVal");
+
+  // var DOMStrings = {
+  //   inputType = "add__type";
+  // }
+  //
+  // return {
+  //   getInput : function() {},
+  //   addListItem : function() {};
+  // }
+  return {
+    getInput : function() { return inputVal.value; },
+    updateUI : function(value) { incomeDisObj.innerHTML =  value; },
+    clearField : function() { inputVal.value = ""; }
+  }
+
+})();
+
+
+var controller = (function() {
+
+  var submitBtnObj = document.getElementById("submitBtn")
+  submitBtnObj.addEventListener("click", function() {
+    budgetController.addIncome(UIController.getInput());
+    UIController.updateUI("+ " + budgetController.actualIncome());
+    UIController.clearField();
+  })
+
+}) ();
